803ce669dfd2f2725f2ab83680e11ccc68105098
cjm2681 <106796849+cjm2681@users.noreply.github.com>
2023-06-03 16:05:27 +0900

    투두리스트

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './components/LoginScreen';
-import SignupScreen from './components/SignupScreen';
+import SignupScreen from './components/SignupScreen'; 
 import CalendarScreen from './components/CalendarScreen'; 
 
 const Stack = createStackNavigator();
