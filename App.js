a0b718721e8edffbbe9a9224efbbbf958c2e24b1
cjm2681 <cjm2681@naver.com>
2023-10-28 14:26:32 +0900

    todolist

--- App.js
+++ App.js
@@ -3,17 +3,25 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './components/LoginScreen';
 import SignupScreen from './components/SignupScreen';
-import CalendarScreen from './components/CalendarScreen';  // <-- 여기에 CalendarScreen 컴포넌트를 추가합니다.
+import CalendarScreen from './components/CalendarScreen';
 
 const Stack = createStackNavigator();
 
 function App() {
     return (
+        // 네비게이션 컨테이너 설정
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Login">
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Signup" component={SignupScreen} />
-                <Stack.Screen name="Calendar" component={CalendarScreen} /> 
+                {/* 로그인 화면 설정 */}
+                <Stack.Screen
+                    name="Login"
+                    component={LoginScreen}
+                    options={{ title: 'ToDoList' }} // 헤더 타이틀을 'ToDoList'로 변경
+                />
+                {/* 회원가입 화면 설정 */}
+                <Stack.Screen name="Signup" component={SignupScreen} />     
+                {/* 캘린더 화면 설정 */}
+                <Stack.Screen name="Calendar" component={CalendarScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
